83f1288b50a0148287ed12e08f1b99523dc58660
axiomizer <nathan.j.jellis@gmail.com>
2023-08-13 11:03:51 -0400

    add test_learning_rate

--- test/unit/test_nn_cfg.py
+++ test/unit/test_nn_cfg.py
@@ -1,4 +1,5 @@
 from test import torchnet
+import random
 import numpy as np
 import torch
 import unittest
@@ -9,6 +10,35 @@ from test.unit.config import *
 
 
 class TestNNConfig(unittest.TestCase):
+    def test_learning_rate(self):
+        filters = 4
+        learning_rates = [10 ** (5 * random.uniform(-1, 0)) for _ in range(10)]
+        for lr in learning_rates:
+            cfg = Config(learning_rate=lr)
+            torch_res = torchnet.TorchResBlock(filters)
+            haida_res = ResidualBlock(filters, cfg)
+            torch_res.copy_trainable_params(haida_res)
+
+            # feedforward for both neural nets and compare results
+            np_in = np.random.randn(MINIBATCH_SIZE, filters, BOARD_SIZE, BOARD_SIZE)
+            torch_in = torch.tensor(np_in, dtype=torch.float64, requires_grad=True)
+            torch_results = torch_res(torch_in)
+            haida_results = haida_res.feedforward(np_in)
+            self.assertTrue(np.allclose(torch_results.detach().numpy(), haida_results))
+
+            # do one step of SGD for both nets and compare results
+            np_target = np.random.randn(MINIBATCH_SIZE, filters, BOARD_SIZE, BOARD_SIZE)
+            loss = torch.nn.MSELoss(reduction='mean')
+            optimizer = torch.optim.SGD(torch_res.parameters(), lr=lr)
+            optimizer.zero_grad()
+            loss(torch_results, torch.tensor(np_target, dtype=torch.float64)).backward()
+            optimizer.step()
+            haida_input_grads = haida_res.backprop(mse_derivative(haida_results, np_target))
+            self.assertTrue(torch_res.compare_params(haida_res))
+
+            # compare gradient with respect to input
+            self.assertTrue(np.allclose(torch_in.grad, haida_input_grads))
+
     def test_weight_decay(self):
         weight_decay = 0.1  # use a value large enough to have a notable effect
         cfg = Config(learning_rate=LEARNING_RATE, weight_decay=weight_decay)
