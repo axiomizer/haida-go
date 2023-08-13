773db595507d82c58191d4e43c5edb10daf2f22e
axiomizer <nathan.j.jellis@gmail.com>
2023-08-13 17:21:04 -0400

    run test_weight_decay 10 times with random weight_decays

--- test/unit/test_nn_cfg.py
+++ test/unit/test_nn_cfg.py
@@ -40,31 +40,35 @@ class TestNNConfig(unittest.TestCase):
             self.assertTrue(np.allclose(torch_in.grad, haida_input_grads))
 
     def test_weight_decay(self):
-        weight_decay = 0.1  # use a value large enough to have a notable effect
-        cfg = Config(learning_rate=LEARNING_RATE, weight_decay=weight_decay)
-        torch_res = torchnet.TorchResBlock(FILTERS)
-        haida_res = ResidualBlock(FILTERS, cfg)
-        torch_res.copy_trainable_params(haida_res)
+        filters = 4
+        learning_rate = 0.1
+        # use values for weight_decay large enough to have a notable effect
+        weight_decays = [10 ** (3 * random.uniform(-1, 0)) for _ in range(10)]
+        for wd in weight_decays:
+            cfg = Config(learning_rate=learning_rate, weight_decay=wd)
+            torch_res = torchnet.TorchResBlock(filters)
+            haida_res = ResidualBlock(filters, cfg)
+            torch_res.copy_trainable_params(haida_res)
 
-        # feedforward for both neural nets and compare results
-        np_in = np.random.randn(MINIBATCH_SIZE, FILTERS, BOARD_SIZE, BOARD_SIZE)
-        torch_in = torch.tensor(np_in, dtype=torch.float64, requires_grad=True)
-        torch_results = torch_res(torch_in)
-        haida_results = haida_res.feedforward(np_in)
-        self.assertTrue(np.allclose(torch_results.detach().numpy(), haida_results))
+            # feedforward for both neural nets and compare results
+            np_in = np.random.randn(MINIBATCH_SIZE, filters, BOARD_SIZE, BOARD_SIZE)
+            torch_in = torch.tensor(np_in, dtype=torch.float64, requires_grad=True)
+            torch_results = torch_res(torch_in)
+            haida_results = haida_res.feedforward(np_in)
+            self.assertTrue(np.allclose(torch_results.detach().numpy(), haida_results))
 
-        # do one step of SGD for both nets and compare results
-        np_target = np.random.randn(MINIBATCH_SIZE, FILTERS, BOARD_SIZE, BOARD_SIZE)
-        loss = torch.nn.MSELoss(reduction='mean')
-        optimizer = torch.optim.SGD(torch_res.parameters(), lr=LEARNING_RATE, weight_decay=weight_decay*2)
-        optimizer.zero_grad()
-        loss(torch_results, torch.tensor(np_target, dtype=torch.float64)).backward()
-        optimizer.step()
-        haida_input_grads = haida_res.backprop(mse_derivative(haida_results, np_target))
-        self.assertTrue(torch_res.compare_params(haida_res))
+            # do one step of SGD for both nets and compare results
+            np_target = np.random.randn(MINIBATCH_SIZE, filters, BOARD_SIZE, BOARD_SIZE)
+            loss = torch.nn.MSELoss(reduction='mean')
+            optimizer = torch.optim.SGD(torch_res.parameters(), lr=learning_rate, weight_decay=wd*2)
+            optimizer.zero_grad()
+            loss(torch_results, torch.tensor(np_target, dtype=torch.float64)).backward()
+            optimizer.step()
+            haida_input_grads = haida_res.backprop(mse_derivative(haida_results, np_target))
+            self.assertTrue(torch_res.compare_params(haida_res))
 
-        # compare gradient with respect to input
-        self.assertTrue(np.allclose(torch_in.grad, haida_input_grads))
+            # compare gradient with respect to input
+            self.assertTrue(np.allclose(torch_in.grad, haida_input_grads))
 
     def test_lr_sched(self):
         cfg = Config(lr_sched=[(0, 0.05), (5, 0.005), (10, 0.0005)])
