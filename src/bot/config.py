8efb4766d823907438b9cfbd0b3c6b8393d31c9a
axiomizer <nathan.j.jellis@gmail.com>
2023-09-16 14:56:04 -0400

    tweak bot config file

--- src/bot/config.py
+++ src/bot/config.py
@@ -1,5 +1,16 @@
+# game settings
+KOMI = 7.5
+BOARD_SIZE = 19
+
+# training
 EPOCHS = 10
 TRAINING_STEPS = 50
+LR_SCHED = [(0,      0.01),
+            (400000, 0.001),
+            (600000, 0.0001)]
+MINIBATCH_SIZE = 128  # 32 on each of 64 workers; 2048 total
+WEIGHT_DECAY = 0.0001  # l2 regularization parameter
+MOMENTUM = 0.9
 
 # self-play settings
 EPISODES = 250  # 25000
@@ -7,19 +18,9 @@ SIMULATIONS = 16  # 1600
 STEPS_SAVED = 20
 C_PUCT = 0.1
 TEMP_THRESHHOLD = 30
-KOMI = 7.5
 
 # neural net architecture
-BOARD_SIZE = 19
-RESIDUAL_BLOCKS = 9  # 19
-HISTORY_PLANES = 6  # 14
+RESIDUAL_BLOCKS = 19
+HISTORY_PLANES = 14
 INPUT_CHANNELS = HISTORY_PLANES + 3
-FILTERS = 16  # 256
-
-# other neural net hyperparameters
-MINIBATCH_SIZE = 32  # 32 on each of 64 workers; 2048 total
-LR_SCHED = [(0,      0.01),
-            (400000, 0.001),
-            (600000, 0.0001)]
-WEIGHT_DECAY = 0.0001  # l2 regularization parameter
-MOMENTUM = 0.9
+FILTERS = 256
